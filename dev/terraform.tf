8ebedd7131412a64304c9f7d32805608e18e46b6
Juan Donado <juan.donado@southworks.com>
2024-02-23 17:10:17 +0100

    Update

--- dev/terraform.tf
+++ dev/terraform.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "tf-plan-jd" {
   name     = "rg-tf-plan-example-centralus"
   location = "centralus"
-  tags = {name: "test1"}
+  tags     = { name : "test1" }
 }
 resource "azurerm_resource_group" "tf-plan-jd-1" {
   name     = "rg-tf-plan-example-centralus"
   location = "centralus"
-  tags = {name: "test2"}
+  tags     = { name : "test2" }
 }
