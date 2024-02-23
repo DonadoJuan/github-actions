645a8c75fa2b2f967d98b9515ca1c3007d8b83f1
Juan Donado <juan.donado@southworks.com>
2024-02-23 16:49:39 +0100

    Update

--- dev/main.tf
+++ dev/main.tf
@@ -9,8 +9,8 @@ terraform {
 
   required_providers {
     azurerm = {
-      source    = "hashicorp/azurerm"
-      version   = "=3.90.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.90.0"
     }
   }
 
